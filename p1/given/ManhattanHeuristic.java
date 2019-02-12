4becee749d3ffad3d42c39422a7b1a1ab16b660e
sonegishi <5055ngs@gmail.com>
2019-02-12 10:54:26 -0500

    Add a distance method in the ManhattanHeuristic class

--- p1/given/ManhattanHeuristic.java
+++ p1/given/ManhattanHeuristic.java
@@ -1,3 +1,5 @@
+import java.lang.Math;
+
 /**
  * This defines the Manhattan heuristic for the 8-Puzzle.
  * @author Steven Bogaerts
@@ -10,9 +12,25 @@ public class ManhattanHeuristic implements Heuristic {
         this.goalState = goalState;
     }
     
+    /**
+     * Returns a distance based on the Manhattan heuristic which is given
+     * by counting the number of movements each tile has to make in order
+     * to get all the tiles to the appropriate positions.
+     */
     public int distance(PuzzleState state) {
-        // *************************** TO DO
-        return -1;
+        int count = 0;
+        for(int i = 1; i < 9; i++) {
+            if(state.getTileAt(i - 1) != i) {
+                int row = (i - 1) / 3;
+                int col = (i - 1) % 3;
+                int currNum = state.posOf(i);
+                int currRow = currNum / 3;
+                int currCol = currNum % 3;
+                int diff = Math.abs(row - currRow) + Math.abs(col - currCol);
+                count += diff;
+            }
+        }
+        return count;
     }
     
 }
