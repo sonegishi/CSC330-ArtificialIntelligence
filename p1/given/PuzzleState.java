928b2dd9267264ae03333ee73a9fd7db42542b63
sonegishi <5055ngs@gmail.com>
2019-02-12 10:45:32 -0500

    Add an expand method and its helper method in the PuzzleState class

--- p1/given/PuzzleState.java
+++ p1/given/PuzzleState.java
@@ -49,8 +49,52 @@ public class PuzzleState {
      * in one move. It's easiest to think of this as moving the blank.
      */
     public LinkedList<PuzzleState> expand() {
-        // *************************** TO DO
-        return null;
+        LinkedList<Integer> idxList = oneMoveIdxList();
+        LinkedList<PuzzleState> list = new LinkedList<>();
+        int zero_idx = posOf(0);
+        for(int idx: idxList) {
+            int[] ints = Arrays.copyOf(tiles, 9);
+            ints[zero_idx] = ints[idx];
+            ints[idx] = 0;
+            PuzzleState state = new PuzzleState(ints);
+            list.add(state);
+        }
+        return list;
+    }
+
+    /**
+     * Returns a list of integers which are going to be swapped with 0.
+     */
+    private LinkedList<Integer> oneMoveIdxList() {
+        LinkedList<Integer> indexList = new LinkedList<>();
+        int zero_idx = posOf(0);
+        if(zero_idx == -1) {
+            return indexList;
+        }
+
+        int row = zero_idx / 3;
+        int col = zero_idx % 3;
+        if(0 <= row - 1) { // Up
+            indexList.add(calcIdx(row - 1, col));
+        }
+        if(row + 1 < 3) {  // Down
+            indexList.add(calcIdx(row + 1, col));
+        }
+        if(0 <= col - 1) { // Left 
+            indexList.add(calcIdx(row, col - 1));
+        }
+        if(col + 1 < 3) { // Right
+            indexList.add(calcIdx(row, col + 1));
+        } 
+        return indexList;
+    }
+
+    /**
+     * Returns an index of a 1-D array calculated from a given row and
+     * column values.
+     */
+    private int calcIdx(int row_idx, int col_idx) {
+        return row_idx * 3 + col_idx;
     }
     
     /**
