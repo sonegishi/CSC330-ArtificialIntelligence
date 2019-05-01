5da9c2d3ae43b9a18cce45fe04b794132ac1935f
keiohtani <35276788+keiohtani@users.noreply.github.com>
2019-05-01 01:05:51 -0400

    Implement computeOutput, train1Example, and trainEpochs

--- p3/p3-given/MultilayerNetwork.java
+++ p3/p3-given/MultilayerNetwork.java
@@ -13,31 +13,31 @@ public class MultilayerNetwork {
     private int numHidden;
     private int numOutput;
     private double learningRate;
-    
+
     /**
      * The weights from the bias node to the hidden layer.
      * bhWeights[a] is the weight from the bias node to hidden node a.
      */
     private double[] bhWeights;
-    
+
     /**
      * The weights from the bias node to the output layer.
      * boWeights[a] is the weight from the bias node to output node a.
      */
     private double[] boWeights;
-    
+
     /**
      * The weights from the input layer to the hidden layer.
      * ihWeights[a][b] is the weight from input node a to hidden node b.
      */
     private double[][] ihWeights;
-    
+
     /**
      * The weights from the hidden layer to the output layer.
      * hoWeights[a][b] is the weight from hidden node a to output node b
      */
     private double[][] hoWeights;
-    
+
     /**
      * Makes a multi-layer neural network with the given specifications.
      */
@@ -46,29 +46,29 @@ public class MultilayerNetwork {
         this.numHidden = numHidden;
         this.numOutput = numOutput;
         this.learningRate = learningRate;
-        
+
         bhWeights = new double[numHidden];
         boWeights = new double[numOutput];
         ihWeights = new double[numInput][numHidden];
         hoWeights = new double[numHidden][numOutput];
-        
+
         RandomWeightGenerator rand = new RandomWeightGenerator(0.1);
-        
+
         for(int hID = 0; hID < numHidden; hID++)
             bhWeights[hID] = rand.generateWeight();
-            
+
         for(int oID = 0; oID < numOutput; oID++)
             boWeights[oID] = rand.generateWeight();
-            
+
         for(int k = 0; k < numInput; k++)
             for(int j = 0; j < numHidden; j++)
                 ihWeights[k][j] = rand.generateWeight();
-        
+
         for(int j = 0; j < numHidden; j++)
             for(int i = 0; i < numOutput; i++)
                 hoWeights[j][i] = rand.generateWeight();
     }
-    
+
     /**
      * Edit this method as you see fit, and then call it with:
      * MultilayerNetwork.makeHardcoded()
@@ -77,20 +77,36 @@ public class MultilayerNetwork {
      */
     public static MultilayerNetwork makeHardcoded() {
         MultilayerNetwork net = new MultilayerNetwork(2, 2, 1, 1);
-        
+
         net.bhWeights = new double[] {0.1, 0.1};
         net.boWeights = new double[] {-0.1};
         net.ihWeights = new double[][] { new double[] {0.1, 0.1}, new double[] {0.1, 0.1} };
         net.hoWeights = new double[][] { new double[] {-0.1}, new double[] {-0.1} };
-        
+
         return net;
     }
-    
+
     public double[] computeOutput(double[] inputs) {
-        // TO DO
-        return new double[] {0.0};
+        double[] outputs = new double[numHidden + numOutput];
+        // calculation from input to hidden layer
+        for (int h = 0; h < numHidden; h++){
+            outputs[h] = -1 * bhWeights[h];
+            for (int i = 0; i < numInput; i++){
+                outputs[h] += inputs[i] * ihWeights[i][h];
+            }
+            outputs[h] = Sigmoid.sig(outputs[h]);
+        }
+        // calculating from hidden layer to output layer
+        for (int o = 0; o < numOutput; o++){
+            outputs[numHidden + o] = -1 * this.boWeights[o];
+            for (int h = 0; h < numHidden; h++){
+                outputs[numHidden + o] += outputs[h] * hoWeights[h][o];
+            }
+            outputs[numHidden + o] = Sigmoid.sig(outputs[numHidden + o]);
+        }
+        return outputs;
     }
-    
+
     /**
      * @param ex The example to train.
      * 
@@ -103,13 +119,45 @@ public class MultilayerNetwork {
      * Compute the new Wk,j weights - the weights from input to hidden.
      */
     public void train1Example(Example ex) {
-        // TO DO
+        double[] outputs = computeOutput(ex.inputs);
+        // Compute err and deltai
+        double err[] = new double[numOutput];
+        double deltai[] = new double[numOutput];
+        for(int i = 0; i < numOutput; i++){
+            err[i] = ex.outputs[i] - outputs[numHidden+i];
+            deltai[i] = err[i] * outputs[numHidden+i] * (1 - outputs[numHidden+i]);
+        }
+        // compute deltaj
+        double deltaj[] = new double[numHidden];
+        double sum = 0;
+        for (int j = 0; j < numHidden; j++){
+            for (int i = 0; i < numOutput; i++){
+                sum += hoWeights[j][i] * deltai[i];
+            }
+            deltaj[j] = outputs[j] * (1-outputs[j]) * sum;
+        }
+        // compute Wj,i
+        for (int i = 0; i < numOutput; i++){
+            boWeights[i] += learningRate * -1 * deltai[i];
+            for (int j = 0; j < numHidden; j++){
+                hoWeights[j][i] += learningRate * outputs[j] * deltai[i];
+            }
+        }
+        // compute Wk,j
+        for (int j = 0; j < numHidden; j++){
+            bhWeights[j] += learningRate * -1 * deltaj[j];
+            for (int k = 0; k < numInput; k++){
+                ihWeights[k][j] += learningRate * ex.inputs[k] * deltaj[j];
+            }
+        }
     }
-    
+
     public void trainEpochs(Example[] examples, int numEpochs) {
-        // TO DO
+        for (int i = 0; i < numEpochs; i++){
+            train1Example(examples[i]);
+        }
     }
-    
+
     /**
      * Computes the output of this network on each of the given examples,
      * and determines the average error on this output.
@@ -122,10 +170,10 @@ public class MultilayerNetwork {
         double[] output;
         for(int exID = 0; exID < examples.length; exID++) {
             output = computeOutput(examples[exID].inputs);
-            
+
             for(int outputID = 0; outputID < numOutput; outputID++)
                 errorSum += Math.abs(examples[exID].outputs[outputID] - output[outputID]);
-                
+
             if (outputLevel == 1)
                 System.out.println("Example " + examples[exID].name + ": " + Arrays.toString(output));
             else if (outputLevel == 2)
@@ -133,37 +181,37 @@ public class MultilayerNetwork {
         }
         System.out.println("Average output node error: " + (errorSum / (examples.length * examples[0].outputs.length)));
     }
-    
+
     /**
      * Returns a String representation of this network.
      */
     public String toString() {
         String result = "bias->hidden: " + Arrays.toString(bhWeights) + "\n";
-        
+
         result += "input->hidden: [  ";
         for(int inputID = 0; inputID < numInput; inputID++)
             result += Arrays.toString(ihWeights[inputID]) + "  ";
         result += "]\n";
-            
+
         result += "bias->output: " + Arrays.toString(boWeights) + "\n";
-        
+
         result += "hidden->output: [  ";
         for(int hiddenID = 0; hiddenID < numHidden; hiddenID++)
             result += Arrays.toString(hoWeights[hiddenID]) + "  ";
         result += "]\n";
-            
+
         return result;
     }
-    
+
     public static void testMultilayerNetwork() {
         MultilayerNetwork net = MultilayerNetwork.makeHardcoded();
         Example ex = new Example(new double[] {1, 0}, new double[] {1});
         System.out.println(net + "\n");
-        
+
         net.train1Example(ex);
         System.out.println(net);
     }
-    
+
     /**
      * This provides further testing of the MultilayerNetwork implementation.
      * This example uses a network with two output units.
@@ -171,40 +219,40 @@ public class MultilayerNetwork {
     public static void testMultilayerNetwork2() {
         // Set up a multilayer network with two outputs, for further testing.
         MultilayerNetwork net = new MultilayerNetwork(2, 2, 2, 1);
-        
+
         net.bhWeights = new double[] {0.9, 0.8};
         net.boWeights = new double[] {0.7, 0.6};
         net.ihWeights = new double[][] { new double[] {0.6, 0.5}, new double[] {0.4, 0.3} };
         net.hoWeights = new double[][] { new double[] {0.2, 0.1}, new double[] {1.1, 1.2} };
-        
+
         // Create the example to be tested.
         Example ex = new Example(new double[] {.8, .4}, new double[] {.3, .7});
-        
+
         // Print the network weights and output before training
         System.out.println("------------------------------\nBefore training\n\n" + net);
         System.out.println("output: " + Arrays.toString(net.computeOutput(ex.inputs)) + "\n");
-        
+
         // Train the network
         net.train1Example(ex);
-        
+
         // Print the network weights and output after training
         System.out.println("------------------------------\nAfter training\n\n" + net);
         System.out.println("output: " + Arrays.toString(net.computeOutput(ex.inputs)) + "\n");
     }
-	
+
     public static void learnXorML(int numEpochs) {
         Example ex1 = new Example(new double[] {0, 0}, new double[] {0});
         Example ex2 = new Example(new double[] {0, 1}, new double[] {1});
         Example ex3 = new Example(new double[] {1, 0}, new double[] {1});
         Example ex4 = new Example(new double[] {1, 1}, new double[] {0});
-        
+
         Example[] examples = new Example[] {ex1, ex2, ex3, ex4};
-        
+
         MultilayerNetwork net = new MultilayerNetwork(2, 2, 1, 0.2);
         net.trainEpochs(examples, numEpochs);
         System.out.println(net);
         net.runTestSet(examples, 2);
     }
-    
+
 }
 
